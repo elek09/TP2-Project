6d71684b646e9fe0ed362715733efabbc00c7b32
ivaanvladimirov <ivanvlad@ucm.es>
2024-02-09 16:06:18 +0100

    Bro we gotta change all the numbers and doubles into final static (constants) to make it better.

--- src/simulator/model/Sheep.java
+++ src/simulator/model/Sheep.java
@@ -3,6 +3,7 @@ package simulator.model;
 import simulator.misc.Vector2D;
 
 public class Sheep extends Animal{
+
     private Animal _danger_source;
     private SelectionStrategy _danger_strategy;
     public Sheep (SelectionStrategy mate_strategy, SelectionStrategy danger_strategy, Vector2D pos){
@@ -10,8 +11,8 @@ public class Sheep extends Animal{
         this._mate_strategy = mate_strategy;
         this._danger_strategy = danger_strategy;
         this._pos = pos;
-        this._speed = 35.0;
-        this._sight_range = 40.0;
+        this._speed = _speedConst;
+        this._sight_range = _sightrangeConst;
     }
     protected Sheep(Sheep p1, Animal p2){
         super(p1, p2);
@@ -23,30 +24,55 @@ public class Sheep extends Animal{
             return;
         }
         if (this._state == State.Normal){
-            //Don't know why it is not working
-            if (distanceTo(_dest) < 8.0){
-                _dest = new Vector2D(Math.random() * 800, Math.random() * 600);
-            }
-            move(this._speed * dt * Math.exp((this._energy - 100.0) * 0.007));
-            this._age += dt;
-            this._energy -= dt * 20.0;
-            assert this._energy > 0 && this._energy <= 100.0;
-            this._desire += 40.0 * dt;
-            assert this._desire > 0 && this._desire <= 100.0;
-            if (this._danger_source == null){
-                //TODO: Search for a new danger source
-
-                if (this._desire > 65.0){
-                    this._state = State.Mate;
-                }
-            }
-            else{
-                this._state = State.Danger;
-            }
+            updateAsNormal(dt);
         }
         if (this._state == State.Danger){
+            updateAsDanger(dt);
 
         }
+
+
+    }
+
+    private void updateAsNormal(double dt) {
+        if (_pos.distanceTo(_dest) < 8.0) {
+            _dest = new Vector2D(Math.random() * 800, Math.random() * 600);
+        }
+        move(this._speed * dt * Math.exp((this._energy - _maxenergy) * _movefactor));
+        this._age += dt;
+        this._energy -= dt * _energyreduction;
+        assert this._energy > _lowestenergy && this._energy <= _maxenergy;
+        this._desire += _desirereduction * dt;
+        assert this._desire > _lowestdesire && this._desire <= _maxdesire;
+        if (this._danger_source == null) {
+            //TODO: Search for a new danger source
+
+            if (this._desire > 65.0) {
+                this._state = State.Mate;
+            }
+        } else {
+            this._state = State.Danger;
+        }
+    }
+    private void updateAsDanger(double dt){
+        if(this._danger_source != null){
+            if(this._state == State.Dead){
+                this._danger_source = null;
+                return;
+            }
+            else {
+                this._dest = _pos.plus(_pos.minus(_danger_source.get_position()).direction());
+                move(2.0 * _speed * dt * Math.exp( (_energy - 100.0 ) * 0.007 ));
+                this._age += dt;
+                this._energy -= 20.0 * 1.2*dt;
+            }
+
+        }
+        else{
+            updateAsNormal(dt);
+        }
+
+
 
 
     }
