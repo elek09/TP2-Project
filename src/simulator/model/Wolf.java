2aefc9429841871a83aa1e657117c3d9df0d2440
elek09 <elekbotond09@gmail.com>
2024-02-14 20:19:44 +0100

    wolf can fuck themselfs with DANGER

--- src/simulator/model/Wolf.java
+++ src/simulator/model/Wolf.java
@@ -1,5 +1,6 @@
 package simulator.model;
 
+import org.json.JSONObject;
 import simulator.misc.Vector2D;
 
 public class Wolf extends Animal{
@@ -31,6 +32,7 @@ public class Wolf extends Animal{
         }
         else if (this._state == State.Danger) {
             updateAsDanger(dt);
+            //An object of type Wolf can never be in the DANGER state.
 
         }
         else if (this._state == State.Mate) {
@@ -62,6 +64,8 @@ public class Wolf extends Animal{
         }
     }
 
+    //i think we dont need this, but we shall discuss it
+    //(An object of type Wolf can never be in the DANGER state.)
     private void updateAsDanger(double dt) {
         if (this._danger_source != null) {
             if (this._state == State.Dead) {
@@ -136,7 +140,61 @@ public class Wolf extends Animal{
 
     private boolean searchForMate() {
         return false;
+    }   //single as fuck sorry bro find a donky u bitch  hahahaha
+
+    /*private void searchForDanger(){}      i think there is no need for this function*/
+
+    /*To find an animal to hunt, it asks the region manager for the list of herbivorous animals in the field of view,
+using the get_animals_in_range method, and then chooses one using the corresponding selection
+strategy.*/
+    private void searchForPrey() {
+        if (this._hunt_target == null) {
+            this._hunt_target = _region_mngr.get_animal_in_range(this, _sight_range, Diet.HERBIVORE, _hunting_strategy);
+        }
     }
 
-    private void searchForDanger(){}
+    public void set_hunting_strategy(SelectionStrategy s) {
+        this._hunting_strategy = s;
+    }
+
+    public SelectionStrategy get_hunting_strategy() {
+        return this._hunting_strategy;
+    }
+
+    //Found it on the internet just kept here for reference maybe there is some stuff which what we can work with
+    public JSONObject as_JSON() {
+        JSONObject ObjectWolf = new JSONObject();
+        ObjectWolf.put("pos", _pos.as_JSON());
+        ObjectWolf.put("energy", _energy);
+        ObjectWolf.put("age", _age);
+        ObjectWolf.put("state", _state.toString());
+        ObjectWolf.put("dest", _dest.as_JSON());
+        ObjectWolf.put("speed", _speed);
+        ObjectWolf.put("sight_range", _sight_range);
+        ObjectWolf.put("desire", _desire);
+        ObjectWolf.put("genetic_code", _genetic_code.as_JSON());
+        ObjectWolf.put("diet", _diet.toString());
+        ObjectWolf.put("mate_strategy", _mate_strategy.toString());
+        ObjectWolf.put("hunting_strategy", _hunting_strategy.toString());
+        if (_baby != null) {
+            ObjectWolf.put("baby", _baby.as_JSON());
+        }
+        if (_mate_target != null) {
+            ObjectWolf.put("mate_target", _mate_target.as_JSON());
+        }
+        if (_hunt_target != null) {
+            ObjectWolf.put("hunt_target", _hunt_target.as_JSON());
+        }
+        return ObjectWolf;
+    }
+
+    public void set_hunt_target(Animal a) {
+        this._hunt_target = a;
+    }
+
+    public Animal get_hunt_target() {
+        return this._hunt_target;
+    }
+
+
 }
