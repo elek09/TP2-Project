abcfc6723db63eb224105701e5c0a335633dd641
elek09 <elekbotond09@gmail.com>
2024-02-15 18:58:38 +0100

    yo puta madre

---
+++ src/simulator/factories/Factory.java
@@ -0,0 +1,10 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+
+import java.util.List;
+
+public interface Factory<T> {
+    T createInstance(JSONObject info) throws IllegalArgumentException;
+    List<JSONObject> getInfo();
+}
