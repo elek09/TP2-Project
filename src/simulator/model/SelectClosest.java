7960eab0ef2ce54ed8200f81a5f91ee1bc850c18
ivaanvladimirov <ivanvlad@ucm.es>
2024-02-28 15:51:15 +0100

    Help, SV, input, output, blablabla in the main and thats it, bottond is acoustic

--- src/simulator/model/SelectClosest.java
+++ src/simulator/model/SelectClosest.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class SelectClosest implements SelectionStrategy{
     @Override
     public Animal select(Animal a, List<Animal> as) {
-        Animal closest = as.get(0);
+        Animal closest = as.getFirst();
         for (Animal animal : as) {
             if (a.get_position().distanceTo(animal.get_position()) < a.get_position().distanceTo(closest.get_position())) {
                 closest = animal;
