5f0788dcbd59b223bac197eac0a27c5eaacbae81
elek09 <elekbotond09@gmail.com>
2024-03-08 20:05:45 +0100

    sheep class overvied sorta, now the sheeps are moving, but there could be more mistakes i dont know. will check it later

--- src/simulator/model/Sheep.java
+++ src/simulator/model/Sheep.java
@@ -18,6 +18,8 @@ public class Sheep extends Animal {
         this._pos = pos;
         this._speed = _speedConst;
         this._sight_range = _sightrangeConst;
+
+        this._danger_source = null;
     }
 
     protected Sheep(Sheep p1, Animal p2) {
@@ -27,6 +29,10 @@ public class Sheep extends Animal {
     }
     @Override
     public void update(double dt) {
+        if (!IsOnTheMap(_pos)) {
+            _pos = _region_mngr.adjust_position(_pos);
+            this._state = State.NORMAL;
+        }
         if (this._state == State.DEAD) {
             return;
         }
@@ -39,115 +45,160 @@ public class Sheep extends Animal {
         else if (this._state == State.MATE) {
             updateAsMate(dt);
         }
-        else if (this._energy==0.0||this._age>8.0){
-            this._state = State.DEAD;
-        }
-        else if (this._state != State.DEAD){
-            _energy += this._region_mngr.get_food(this,dt);
+        else if (_energy<=0.0 || _age>8.0){
+            _state = State.DEAD;
         }
 
+
+        //ensuring its value remains between 0.0 and 100.0      IMPORTANT
+        if (_energy > _maxenergy) {
+            _energy = _maxenergy;
+        } else if (_energy < 0) {
+            _energy = 0;
+        }
+        else {
+            _energy += this._region_mngr.get_food(this,dt);
+        }
     }
 
     //Checked
     private void updateAsNormal(double dt) {
         if (_pos.distanceTo(_dest) < 8.0) {
-            _dest = new Vector2D(Math.random() * 800, Math.random() * 600);
+            _dest = new Vector2D(Math.random() * _region_mngr.get_width(), Math.random() * _region_mngr.get_height());
         }
-        move(this._speed * dt * Math.exp((this._energy - _maxenergy) * _movefactor));
-        this._age += dt;
-        this._energy -= dt * _energyreductionSheep;
-        assert this._energy > _lowestenergy && this._energy <= _maxenergy;
-        this._desire += _desirereductionSheep * dt;
-        assert this._desire > _lowestdesire && this._desire <= _maxdesire;
+        move(_speed * dt * Math.exp((this._energy - _maxenergy) * _movefactor));
+        _age += dt;
+
+        //IMPORTANT
+        if (_energy > _maxenergy) {
+            _energy = _maxenergy;
+        } else if (_energy < 0) {
+            _energy = 0;
+        }
+        else {
+            _energy -= dt * _energyreductionSheep;
+        }
+
+        if(_desire > _maxdesire){
+            _desire = _maxdesire;
+        }
+        else if(_desire < _lowestdesire){
+            _desire = _lowestdesire;
+        }
+        else {
+            _desire += _desirereductionSheep * dt;
+        }
+
         if (this._danger_source == null) {
             searchForDanger(_region_mngr, this._danger_strategy);
+        }
 
-            if (this._desire > _desireUpperBound) {
-                this._state = State.MATE;
-            }
-        } else {
+        if (this._danger_source != null) {
             this._state = State.DANGER;
+        }
+        else if (_danger_source == null && _desire > 65.0) {
+            this._state = State.MATE;
         }
     }
 
 
     private void updateAsDanger(double dt) {
-        if (this._danger_source != null) {
-            if (this._state == State.DEAD) {
-                this._danger_source = null;
-                return;
-            } else {
-                this._dest = _pos.plus(_pos.minus(_danger_source.get_position()).direction());
-                move(_speedFactorSheep * _speed * dt * Math.exp((_energy - _maxenergy) * _multiplicativeMath));
-                this._age += dt;
+        if (_danger_source != null && _danger_source.get_state() == State.DEAD) {
+            _danger_source = null;
+        }
+        if (_danger_source == null) {
+            this._state = State.NORMAL;
+            return;
+        }
 
-                this._energy -= _energyreductionSheep * _multiplicativeTime * dt;
-                assert this._energy > _lowestenergy && this._energy <= _maxenergy;
+        if (_danger_source != null) {
+            _dest = _pos.plus(_pos.minus(_danger_source.get_position()).direction());
+            move(_speedFactorSheep * _speed * dt * Math.exp((_energy - _maxenergy) * _multiplicativeMath));
+            this._age += dt;
 
+            //IMPORTANT
+            if (_energy > _maxenergy) {
+                _energy = _maxenergy;
+            } else if (_energy < 0) {
+                _energy = 0;
+            }
+            else {
+                _energy -= _energyreductionSheep * _multiplicativeTime * dt;
+            }
+
+            if(_desire > _maxdesire){
+                _desire = _maxdesire;
+            }
+            else if(_desire < _lowestdesire){
+                _desire = _lowestdesire;
+            }
+            else {
                 this._desire += _desirereductionSheep * dt;
-                assert this._desire > _lowestdesire && this._desire <= _maxdesire;
             }
         } else {
-            if (this._desire >= _desireUpperBound) {
-                this._state = State.MATE;
-            } else {
-                this._state = State.NORMAL;
+            _danger_source=searchForDanger(_region_mngr, this._danger_strategy);
+
+            if (_danger_source == null) {
+                if (_desire < _desireUpperBound) {
+                    _state = State.NORMAL;
+                } else {
+                    _state = State.MATE;
+                }
             }
-            searchForDanger(_region_mngr, this._danger_strategy);
-            updateAsNormal(dt);
         }
 
 
     }
 
     private void updateAsMate(double dt) {
-        if (this._mate_target != null) {
-            if (this._state == State.DEAD || this._sight_range < _pos.distanceTo(_mate_target.get_position())) {
-                this._mate_target = null;
-                return;
-            } else {
-
-
-            }
-        } else if (this._mate_target == null)
-            //Searches for a mate and if there is no mate, it will update as normal
-            if (searchForMate(_region_mngr, this._mate_strategy) == null) {
+        if (this._mate_target != null && (this._state == State.DEAD || this._sight_range < _pos.distanceTo(_mate_target.get_position()))) {
+            this._mate_target = null;
+        } else if (this._mate_target == null) {
+            _mate_target=searchForMate(_region_mngr, this._mate_strategy);
+            if  (_mate_target == null) {
                 updateAsNormal(dt);
             } else {
-                this._dest = _mate_target.get_position();
+                _dest = _mate_target.get_position();
                 move(_speedFactorSheep * dt * Math.exp((_energy - _maxenergy) * _multiplicativeMath));
                 this._age += dt;
 
-                this._energy -= _energyreductionSheep * _multiplicativeTime * dt;
-                assert this._energy > _lowestenergy && this._energy <= _maxenergy;
+                //IMPORTANT
+                if (_energy > _maxenergy) {
+                    _energy = _maxenergy;
+                } else if (_energy < 0) {
+                    _energy = 0;
+                } else {
+                    _energy -= _energyreductionSheep * _multiplicativeTime * dt;
+                }
 
-                this._desire += _desirereductionSheep * dt;
-                assert this._desire > _lowestdesire && this._desire <= _maxdesire;
+                if (_desire > _maxdesire) {
+                    _desire = _maxdesire;
+                } else if (_desire < _lowestdesire) {
+                    _desire = _lowestdesire;
+                } else {
+                    this._desire += _desirereductionSheep * dt;
+                }
+
 
                 if (this._pos.distanceTo(_mate_target.get_position()) < 8.0) {
                     this._desire = 0;
                     _mate_target._desire = 0;
-                    if (this._baby == null && Math.random() < 0.9) {
-                        this._baby = new Sheep(this, _mate_target);
+                    if (!is_pregnant() && Math.random() < 0.9) {
+                        _baby = new Sheep(this, _mate_target);
                     }
-                    this._state = State.NORMAL;
+                    _mate_target = null;
                 }
-
             }
-            else if (this._danger_source != null){
-                this._state = State.DANGER;
         }
-            else if (this._danger_source == null){
-                searchForDanger(_region_mngr, this._danger_strategy);
-            }
-            else if (this._danger_source != null){
-                if (this._desire < _desireUpperBound){
-                    this._state = State.NORMAL;
-                }
-                else {
-                    this._state = State.DANGER;
-                }
-            }
+        if (this._danger_source == null){
+            _danger_source=searchForDanger(_region_mngr, this._danger_strategy);
+        }
+        if (this._danger_source != null){
+            _state = State.DANGER;
+        }
+        if (this._danger_source == null && this._desire < _desireUpperBound){
+            this._state = State.NORMAL;
+        }
     }
 
     @Override
