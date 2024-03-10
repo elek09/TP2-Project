e580a90690f7f1474fda404cdfbb7c1945a3cf0e
ivaanvladimirov <ivanvlad@ucm.es>
2024-03-10 17:44:38 +0100

    object viewer works

--- src/simulator/model/SelectClosest.java
+++ src/simulator/model/SelectClosest.java
@@ -7,13 +7,16 @@ import java.util.List;
 public class SelectClosest implements SelectionStrategy{
     @Override
     public Animal select(Animal a, List<Animal> as) {
-        Animal closest = as.getFirst();
-        for (Animal animal : as) {
-            if (a.get_position().distanceTo(animal.get_position()) < a.get_position().distanceTo(closest.get_position())) {
-                closest = animal;
+        if(!as.isEmpty()){
+            Animal closest = as.getFirst();
+            for (Animal animal : as) {
+                if (a.get_position().distanceTo(animal.get_position()) < a.get_position().distanceTo(closest.get_position())) {
+                    closest = animal;
+                }
             }
+            return closest;
         }
-        return closest;
+        return null;
     }
 
     @Override
